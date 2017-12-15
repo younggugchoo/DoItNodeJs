db4baf69424bf603f13dc0ae7cdb053f98572c6f
younggugchoo <30790629+younggugchoo@users.noreply.github.com>
2017-12-15 13:43:37 +0900

    Initial commit

---
+++ calc2.js
@@ -0,0 +1,10 @@
+var calc = {};
+
+calc.add = function (a , b) {
+    return a +b ;
+}
+
+module.exports = calc;
+
+
+
---
+++ ch02_test.js
@@ -0,0 +1,7 @@
+var calc = require('./calc');
+console.log(calc.add(120, 120) );
+
+
+var calc2 = require('./calc2')
+
+console.log(calc2.add(12, 22));
